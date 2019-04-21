8accd105b44c44b8079b017b82cb8ee96eb66f85
Hamza <hamza_razeq@hotmail.co.uk>
2019-04-21 14:48:41 +0100

    added findAndReplace method to MyString class

--- src/MyString.java
+++ src/MyString.java
@@ -2,10 +2,38 @@ public class MyString {
 
 	public static void main(String[] args) {
 
-		find("Tomtom is Tomtom but okT", "Tom");
+		//find("Tomtom is Tomtom but okT", "Tom");
+		findAndReplace("Tomtom is Tom and not Tom","Tom","$$");
 
 	}
+	
+	public static void findAndReplace(String msg, String lookfor, String replace){
+		int i;
+		i = 0;
+		String newmsg = "";
+		for (; i < msg.length();i++) {
+			if ((msg.length()-i)>=lookfor.length()) {
+				if (msg.substring(i, i + 1).toLowerCase().equals(lookfor.substring(0, 1).toLowerCase())) {
+					if (msg.substring(i, i + lookfor.length()).toLowerCase().equals(lookfor.toLowerCase())) {
+						newmsg = newmsg + replace;
+						i = i + lookfor.length() - 1;
 
+					}
+				}
+				else{
+					newmsg =  newmsg + msg.substring(i,i+1);
+				}
+			}
+			
+		}
+
+		System.out.println(newmsg);
+	}
+
+	
+	
+	
+	
 	public static void find(String msg, String lookfor) {
 		int i, found;
 		i = found = 0;
