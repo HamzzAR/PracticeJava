3f41221ad94f38b65e38affa0900289d1d286f5a
Hamza <hamza_razeq@hotmail.co.uk>
2019-04-21 15:40:23 +0100

    Bug fixed in findAndReplace method

--- src/MyString.java
+++ src/MyString.java
@@ -2,47 +2,46 @@ public class MyString {
 
 	public static void main(String[] args) {
 
-		//find("Tomtom is Tomtom but okT", "Tom");
-		findAndReplace("Tomtom is Tom and not Tom","Tom","$$");
+		// find("Tomtom is Tomtom but okT", "Tom");
+		findAndReplace("Tomptom is TTT Toms and not TomK", "Tom", "*");
 
 	}
-	
-	public static void findAndReplace(String msg, String lookfor, String replace){
+
+	public static void findAndReplace(String msg, String lookfor, String replace) {
 		int i;
 		i = 0;
 		String newmsg = "";
-		for (; i < msg.length();i++) {
-			if ((msg.length()-i)>=lookfor.length()) {
-				if (msg.substring(i, i + 1).toLowerCase().equals(lookfor.substring(0, 1).toLowerCase())) {
+		for (; i < msg.length(); i++) {
+			if (msg.substring(i, i + 1).toLowerCase().equals(lookfor.substring(0, 1).toLowerCase())) {
+				if ((msg.length() - i) >= lookfor.length()) {
 					if (msg.substring(i, i + lookfor.length()).toLowerCase().equals(lookfor.toLowerCase())) {
 						newmsg = newmsg + replace;
 						i = i + lookfor.length() - 1;
 
+					} else {
+						newmsg = newmsg + msg.substring(i, i + 1);
 					}
 				}
-				else{
-					newmsg =  newmsg + msg.substring(i,i+1);
-				}
+			} else {
+				newmsg = newmsg + msg.substring(i, i + 1);
 			}
-			
+
 		}
 
 		System.out.println(newmsg);
 	}
 
-	
-	
-	
-	
 	public static void find(String msg, String lookfor) {
 		int i, found;
 		i = found = 0;
 		for (; i < msg.length(); i++) {
-			if ((msg.length()-i)>=lookfor.length()) {
-				if (msg.substring(i, i + 1).toLowerCase().equals(lookfor.substring(0, 1).toLowerCase())) {
-					if (msg.substring(i, i + lookfor.length()).toLowerCase().equals(lookfor.toLowerCase())) {
+			if ((msg.length() - i) >= lookfor.length()) {
+				if (msg.substring(i, i + 1).toLowerCase()
+						.equals(lookfor.substring(0, 1).toLowerCase())) {
+					if (msg.substring(i, i + lookfor.length()).toLowerCase()
+							.equals(lookfor.toLowerCase())) {
 						found++;
-					//	System.out.println("Found one");
+						// System.out.println("Found one");
 						i = i + lookfor.length() - 1;
 
 					}
