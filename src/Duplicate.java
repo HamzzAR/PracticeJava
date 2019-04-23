5752beac6db68c23b869647928c4983bed9c95a4
Hamza <hamza_razeq@hotmail.co.uk>
2019-04-23 17:42:01 +0100

    files updated

--- src/Duplicate.java
+++ src/Duplicate.java
@@ -3,54 +3,31 @@ public class Duplicate {
 
 
 	public static void main(String[] args) {
-		message("hell hell hell is this is too");
+		message("hell hell is this is too");
 		
 
 	}
 	
 	public static void message(String msg) {
+		String letter = "";
 		String word = "";
+		String newMsg = "";
 		for(int x=0; x<msg.length(); x++){
-			String letter = msg.substring(x,x+1);
+			letter = msg.substring(x,x+1);
 			if(letter.equals(" ")){
-				String r = removeDuplicate(word,msg);
-				System.out.println(r);
-				word = "";
+				msg = removeDuplicate(word,msg.substring(x+1,msg.length()));
+				word+=letter;
+				newMsg+=word;
 			}
-			word+=letter;
+			else{
+				word+=letter;
+			}
 		}
-		
-		
 
 	}
 	
-	public static String removeDuplicate(String word, String newMsg){
-		String msg = "";
-		String word2 = "";
-		boolean firttime = true;
-		for(int x=0; x<newMsg.length(); x++){
-			String letter = newMsg.substring(x,x+1);
-			if(letter.equals(" ")){
-				if(!word2.equals(word)){
-					msg+=word2;
-					word2 = "";
-					firttime = false;
-				}else{
-					if(firttime){
-						msg+=word2;
-						word2="";
-						firttime = false;
-					}else{
-						word2="";
-					}
-				}
-				word2+=letter;
-			}else{
-				word2+=letter;
-			}
-		}
-		
-		return msg;
+	public static String removeDuplicate(String word, String msg){
+	
 		
 	}
 
