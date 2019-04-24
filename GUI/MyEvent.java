555ed68be98da7060a497466582dda07cb59b1ea
Hamza <hamza_razeq@hotmail.co.uk>
2019-04-24 12:25:33 +0100

    added GUI folder

---
+++ GUI/MyEvent.java
@@ -0,0 +1,11 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class MyEvent implements ActionListener{
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.println("Hello my friends");
+		
+	}
+}
--- GUI/Windows.java
+++ GUI/Windows.java
@@ -1,11 +1,23 @@
-import java.awt.Frame;
+import java.awt.*;
 public class Windows {
 
 	public static void main(String[] args) {
-		Frame f = new Frame();
+		Frame f = new Frame("QA Cunsulting");
+		Button b1,b2,b3,b4,b5;
+		b1 = new Button("Click 1");
+		b2 = new Button("Click 2");
+		b3 = new Button("Click 3");
+		b4 = new Button("Click 4");
+		b5 = new Button("Click 5");
+		MyEvent action = new MyEvent();
+		b1.addActionListener(action);
+		f.add(b1,BorderLayout.NORTH);
+		f.add(b2,BorderLayout.SOUTH);
+		f.add(b3,BorderLayout.EAST);
+		f.add(b4,BorderLayout.WEST);
+		f.add(b5,BorderLayout.CENTER);
+		f.setSize(500,500);
 		f.setVisible(true);
-		f.dispose();
-		
 	}
 
-}
+} 
